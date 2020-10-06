d205ba5f49de0242cc4a35a518bc477d6b14451c
Cris Perez <cristino.perez525@gmail.com>
2020-10-06 16:38:27 -0500

    Completed burgermodel.js file

---
+++ models/burgermodel.js
@@ -0,0 +1,24 @@
+var orm = require("../config/orm.js")
+
+var burger = {
+
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(result){
+            cb(result)
+        });
+    },
+
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(result) {
+            cb(result)
+        })
+    },
+
+    updateOne: function(vals, condition, cb) {
+        orm.insertOne("burgers", vals, condition, function(result) {
+            cb(result)
+        });
+    }
+}
+
+module.exports = burger
