d70e2a1f845ab13863a7f091193b7f93faa929b7
Cris Perez <cristino.perez525@gmail.com>
2020-10-08 14:19:19 -0500

    removed test database password

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Pookie_25?!',
+    password: '',
     database: 'burger_db'
   });
 };
